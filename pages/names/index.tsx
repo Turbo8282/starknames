6c3ea15877934b60283bf2955aa1f3550cafc214
Alex Wang PC <alexwang2882@gmail.com>
2022-07-27 22:30:19 +1000

    useEffect error

--- pages/names/index.tsx
+++ pages/names/index.tsx
@@ -1,10 +1,16 @@
 import { useRouter } from "next/router";
-  
+import { useEffect } from "react";
 
 export default function NameIndex() {
     const router = useRouter();
     const name = router.query.name;
+    useEffect(() => {
+        try {
+            router.push("/names/" + name + "/register")
+        } catch {
+          router.push("/");
+        }
+      }, []);
 
-    router.push("/names/" + name + "/register")
     
 }
